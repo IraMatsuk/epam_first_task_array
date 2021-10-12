9ee8af07bf0caec6691cfcf7ca9513af952895a9
IraMatsuk <47867050+IraMatsuk@users.noreply.github.com>
2021-10-12 12:01:23 +0300

    Revert "feat: add constructors to ArrayException class"

--- src/main/java/by/matsukiryna/arraytask/exception/ArrayException.java
+++ src/main/java/by/matsukiryna/arraytask/exception/ArrayException.java
@@ -1,19 +1,7 @@
 package by.matsukiryna.arraytask.exception;
 
 public class ArrayException  extends Exception{
-    public ArrayException() {
-    }
-
     public ArrayException(String message) {
         super(message);
     }
-
-    public ArrayException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ArrayException(Throwable cause) {
-        super(cause);
-    }
-
 }
